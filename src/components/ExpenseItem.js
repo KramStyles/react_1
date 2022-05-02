bbffc5577b4679982ed4b9b65b8180d0f9de053b
kramstyles <mark.eke@decagon.dev>
2022-05-02 03:40:15 +0100

    added expensesitem.css

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+    return (
+        <div>
+            <h5>May 2<sup>nd</sup>, 2022</h5>
+            <div>
+                <h3>Peanuts</h3>
+                <p>$3.99</p>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
