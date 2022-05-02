ba7c0b886ff19ea17c8a54545e7a4ab6ce3fd8f0
kramstyles <mark.eke@decagon.dev>
2022-05-02 23:25:33 +0100

    worked on date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,18 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+    let month = props.date.toLocaleString('en-US', {month: 'long'});
+    let day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    let year = props.date.getFullYear();
+
 
     return (
         <div className={"expense_container"}>
-            <h2>{props.date.toISOString()}</h2>
+            <pre>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </pre>
             <div>
                 <h3>{props.title} <sup>{props.id}</sup></h3>
                 <p>${props.amount}</p>
