55975d598cc4862941041ef90fe7e00708025131
kramstyles <mark.eke@decagon.dev>
2022-05-02 23:00:30 +0100

    introduced props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+    const expenses = [
+        {'id': 1, 'title': 'Toilet Paper', amount: 0.99, 'date': new Date(2021, 11, 12)},
+        {'id': 2, 'title': 'Groceries', amount: 17.99, 'date': new Date(2021, 1, 2)},
+        {'id': 3, 'title': 'Internet', amount: 10.49, 'date': new Date(2021, 4, 5)},
+        {'id': 4, 'title': 'Leisure', amount: 5.99, 'date': new Date(2021, 11, 12)},
+    ]
     return (
         <div>
             <h1>Hello and Good morning</h1>
-            <ExpenseItem/>
+            <ExpenseItem title={expenses[0].title} id={expenses[0].id} date={expenses[0].date} amount={expenses[0].amount}/>
         </div>
     );
 }
