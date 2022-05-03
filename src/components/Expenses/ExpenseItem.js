f7fc6c9ebf0e33376f94637b084288b2c0e913fc
kramstyles <mark.eke@decagon.dev>
2022-05-03 14:59:33 +0100

    worked with useState()

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,25 @@
+import {useState} from "react";
+
 import ExpenseDate from "./ExpenseDate";
 
 import './ExpenseItem.css';
 import Card from "../UI/Card";
+
 const ExpenseItem = (props) => {
+    let [title, setTitle] = useState(props.title); //Used to update dom elements
+    const changeTitle = () =>{
+        setTitle('HellΩΩ')
+    }
 
     return (
         <Card className={"expense_container"}>
             <ExpenseDate date={props.date}/>
             <div className={'div'}>
-                <h3>{props.title} <sup>{props.id}</sup></h3>
-                <p className={'p'}>${props.amount}</p>
+                <h3>{title} <sup>{props.id}</sup></h3>
+                <div>
+                    <p className={'p'}>${props.amount}</p>
+                    <button onClick={changeTitle}>Change Title</button>
+                </div>
             </div>
         </Card>
     );
