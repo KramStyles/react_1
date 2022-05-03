1f90ff64521703b5ae6194336b3da42aebbaee70
kramstyles <mark.eke@decagon.dev>
2022-05-03 20:48:03 +0100

    introduced mutiple states in a better way

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -3,14 +3,32 @@ import {useState} from "react";
 import './ExpenseForm.css'
 
 export default function ExpenseForm() {
-    const [title, setTitle] = useState('');
-    const [amount, setAmount] = useState('');
-    const [date, setDate] = useState('');
+    // const [title, setTitle] = useState('');
+    // const [amount, setAmount] = useState('');
+    // const [date, setDate] = useState('');
+    //
+    //
+    // const TitleWatcher = (e) => setTitle(e.target.value);
+    // const AmountWatcher = (e) => setAmount(e.target.value);
+    // const DateWatcher = (e) => setDate(e.target.value);
 
+    const [userInput, setUserInput] = useState({
+        'userTitle': '',
+        'userAmount': '',
+        'userDate': '',
+    });
 
-    const TitleWatcher = (e) => setTitle(e.target.value);
-    const AmountWatcher = (e) => setAmount(e.target.value);
-    const DateWatcher = (e) => setDate(e.target.value);
+    const TitleWatcher = (e) => {
+        setUserInput({
+                ...userInput, 'userTitle': e.target.value
+            });
+    }
+    const AmountWatcher = (e) => {
+        setUserInput({...userInput, 'userAmount': e.target.value});
+    }
+    const DateWatcher = (e) => {
+        setUserInput({...userInput, 'userDate': e.target.value});
+    }
 
     return (
         <form action="" className={'expense_form'}>
