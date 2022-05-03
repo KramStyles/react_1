59f3e44294d7b968302dac1e863c8f04a5251b2a
kramstyles <mark.eke@decagon.dev>
2022-05-03 20:34:02 +0100

    added multiple states

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,19 +1,30 @@
+import {useState} from "react";
+
 import './ExpenseForm.css'
 
 export default function ExpenseForm() {
+    const [title, setTitle] = useState('');
+    const [amount, setAmount] = useState('');
+    const [date, setDate] = useState('');
+
+
+    const TitleWatcher = (e) => setTitle(e.target.value);
+    const AmountWatcher = (e) => setAmount(e.target.value);
+    const DateWatcher = (e) => setDate(e.target.value);
+
     return (
         <form action="" className={'expense_form'}>
             <div className="form_input">
                 <label htmlFor="txtInput">Title</label>
-                <input type="text" id={'txtInput'}/>
+                <input type="text" id={'txtInput'} onChange={TitleWatcher}/>
             </div>
             <div className="form_input">
                 <label htmlFor="txtAmount">Amount</label>
-                <input type="number" id={'txtAmount'} min={'0.01'} step={'0.01'}/>
+                <input type="number" id={'txtAmount'} min={'0.01'} step={'0.01'} onChange={AmountWatcher}/>
             </div>
             <div className="form_input">
                 <label htmlFor="txtDate">Date</label>
-                <input type="date" id={'txtDate'} min={'2018-01-01'} max={'2023-12-12'}/>
+                <input type="date" id={'txtDate'} min={'2018-01-01'} max={'2023-12-12'} onChange={DateWatcher}/>
             </div>
             <div className={'form_submit'}>
                 <input type="button" value="Add Expense"/>
