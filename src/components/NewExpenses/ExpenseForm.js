e0a07f21c8ad1f070435f81bcf367cfecf96d8b4
kramstyles <mark.eke@decagon.dev>
2022-05-03 21:18:00 +0100

    cleaned inputs on submit

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -3,16 +3,6 @@ import {useState} from "react";
 import './ExpenseForm.css'
 
 export default function ExpenseForm() {
-    const [title, setTitle] = useState('');
-   const [amount, setAmount] = useState('');
-   const [date, setDate] = useState('');
-
-
-   const TitleWatcher = (e) => setTitle(e.target.value);
-   const AmountWatcher = (e) => setAmount(e.target.value);
-   const DateWatcher = (e) => setDate(e.target.value);
-
-
     // const [userInput, setUserInput] = useState({
     //     'userTitle': '',
     //     'userAmount': '',
@@ -33,6 +23,14 @@ export default function ExpenseForm() {
     //     })
     // }
 
+    const [title, setTitle] = useState('');
+    const [amount, setAmount] = useState('');
+    const [date, setDate] = useState('');
+
+    const TitleWatcher = (e) => setTitle(e.target.value);
+    const AmountWatcher = (e) => setAmount(e.target.value);
+    const DateWatcher = (e) => setDate(e.target.value);
+
     const SubmitHandler = (e) => {
         e.preventDefault();
         const expenseData = {
@@ -41,21 +39,24 @@ export default function ExpenseForm() {
             'date': new Date(date),
         }
         console.log(expenseData)
+        setTitle('')
+        setAmount('')
+        setDate('')
     }
 
     return (
         <form onSubmit={SubmitHandler} className={'expense_form'}>
             <div className="form_input">
                 <label htmlFor="txtInput">Title</label>
-                <input type="text" id={'txtInput'} onChange={TitleWatcher}/>
+                <input type="text" id={'txtInput'} onChange={TitleWatcher} value={title}/>
             </div>
             <div className="form_input">
                 <label htmlFor="txtAmount">Amount</label>
-                <input type="number" id={'txtAmount'} min={'0.01'} step={'0.01'} onChange={AmountWatcher}/>
+                <input type="number" id={'txtAmount'} min={'0.01'} step={'0.01'} onChange={AmountWatcher} value={amount}/>
             </div>
             <div className="form_input">
                 <label htmlFor="txtDate">Date</label>
-                <input type="date" id={'txtDate'} min={'2018-01-01'} max={'2023-12-12'} onChange={DateWatcher}/>
+                <input type="date" id={'txtDate'} min={'2018-01-01'} max={'2023-12-12'} onChange={DateWatcher} value={date}/>
             </div>
             <div className={'form_submit'}>
                 <input type="submit" value="Add Expense"/>
